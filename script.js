4ee38d6e488281763dfb16a8a87f968e39ed827b
Rodrigo <219222439+CafeteriaLaEstacion@users.noreply.github.com>
2025-07-04 01:49:08 -0400

    fix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function showPage(pageNumber) {
   const pages = document.querySelectorAll('.menu-page');
   const footer = document.getElementById('site-footer');
+  const header = document.querySelector('header'); // 👉 referencia al header
 
   pages.forEach((page, index) => {
     if (index === pageNumber - 1) {
@@ -10,10 +11,13 @@ function showPage(pageNumber) {
     }
   });
 
-  // Mostrar el footer solo en la página principal (page1)
+  // Mostrar el footer solo en la página principal (page 1)
   if (pageNumber === 1) {
     footer.style.display = 'flex';
+    header.classList.remove('hidden'); // 👉 mostrar header
   } else {
     footer.style.display = 'none';
+    header.classList.add('hidden'); // 👉 ocultar header
   }
+  window.scrollTo(0, 0);
 }
